ea2ae4394f2f10e3d815b1f3b2d4a41a3db028d8
PYDAH <pydah.edu@gmail.com>
2025-08-16 07:29:50 +0000

    this is logo image upload this image as logo

--- src/components/gethub-logo.tsx
+++ src/components/gethub-logo.tsx
@@ -5,11 +5,10 @@ export default function GethubLogo(props: Partial<ImageProps>) {
   const { width = 200, height = 200, ...rest } = props;
   return (
     <Image
-      src="https://placehold.co/200x200.png"
+      src="https://storage.googleapis.com/res-studio-user-images/projects/f2283e78-3844-41f2-9599-f47a61d15c1b/ventura_1722330366885_616859_processed.png"
       alt="GETHUB Logo"
       width={width}
       height={height}
-      data-ai-hint="logo"
       {...rest}
     />
   );
