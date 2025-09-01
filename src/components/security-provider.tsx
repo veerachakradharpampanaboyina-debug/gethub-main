d6361b430ecebd88f3eb6dc3f16c416dae750a2e
PYDAH <pydah.edu@gmail.com>
2025-09-01 08:34:27 +0000

    now disable that when i give instruction then activate

--- src/components/security-provider.tsx
+++ src/components/security-provider.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 export default function SecurityProvider({ children }: { children: React.ReactNode }) {
   // Activate the security hook. It will run its checks and handle redirection.
-  useSecurity();
+  // useSecurity();
 
   // Render the children of the component.
   return <>{children}</>;
